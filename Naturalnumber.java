24bb9b906a80aacc53abeef197dfdac84583b16d
731614105005 <bawadharani96@gmail.com>
2017-07-05 09:57:29 +0530

    Create Naturalnumber.java

---
+++ Naturalnumber.java
@@ -0,0 +1,17 @@
+# set9
+import java.io.*;
+ import java.util.*;
+ public class Naturalnumber
+ {
+   public static void main(String args[])throws IOException
+   {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+     int i,sum=0;
+     for(i=1;i<=n;i++)
+     {
+        sum=sum+i;
+        }
+        System.out.println(sum);
+       }
+       }
